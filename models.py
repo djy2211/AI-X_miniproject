2a28c42690d0db531e6f1aa7e1656cc55262576b
djy2211 <131187694+djy2211@users.noreply.github.com>
2024-02-08 12:52:27 +0900

    Update models.py

--- models.py
+++ models.py
@@ -2,9 +2,9 @@ from email.policy import default
 from sqlalchemy import Column, Integer, LargeBinary, String
 from database import Base
 
-class User(Base):
+class User(Base): # 사진 이름과 경로를 저장하기 위해서 모델 클래스 생성
     __tablename__ = "users"
 
     user_id = Column(Integer, primary_key=True)
-    user_name = Column(String(100))
-    user_image = Column(String(1000))
+    user_name = Column(String(100)) # 사진 이름
+    user_image = Column(String(1000)) # 이미지 경로
